67625dda9ce6ae11761dc76aeb48f10581b72140
Luscas-nl <eduardol123azq@gmail.com>
2023-09-05 16:48:40 -0300

    First Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import GeelLogo from "../public/LogoGeel.png"
+import Back from "../public/Background.png"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="AppInner">
+      <div className="backInner">
+        <img src={Back} alt="" />
+      </div>
       <div className="reportInner">
         <div className="imgInner">
           <img src={GeelLogo} alt="" />
         </div>
-        <textarea className='reportArea' />
-        <button className="reportSend">Enviar Denúncia</button>
+        <h1 className="reportTheme">CANAL DE DENÚNCIAS</h1>
+        <textarea className='reportArea' placeholder='Digite seu relato...'/>
+        <button className="reportSend" >Enviar Denúncia</button>
       </div>
     </div>
   )
