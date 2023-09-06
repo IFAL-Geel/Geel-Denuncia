15c636e73f88464a3c24d762c7759e0dbe2bcd69
Luscas-nl <eduardol123azq@gmail.com>
2023-09-06 20:58:26 -0300

    Identity Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,7 +6,16 @@ import GeelLogo from "../public/LogoGeel.png"
 import Back from "../public/Background.png"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("")
+  const [sendEnabled, setSendEnabled] = useState(false)
+
+  useEffect(() => {
+    if(message.trim() !== ""){
+      setSendEnabled(true)
+    } else {
+      setSendEnabled(false)
+    }
+  })
 
   return (
     <div className="AppInner">
@@ -14,12 +23,34 @@ function App() {
         <img src={Back} alt="" />
       </div>
       <div className="reportInner">
-        <div className="imgInner">
-          <img src={GeelLogo} alt="" />
+        <form action="" className="formInner">
+          <div className="imgInner">
+            <img src={GeelLogo} alt="" />
+          </div>
+          <h1 className="reportTheme">CANAL DE DENÚNCIAS</h1>
+          <div className="nameInner">
+            <input
+            name='Nome'
+            className='reportName'
+            type="text" 
+            placeholder='Digite seu nome ou deixe vazio para "Anônimo"'/>
+            <div className="iconInner">
+              <i class="fa-solid fa-user"></i>
+            </div>
+          </div>
+          <textarea
+            name='Relato'
+            value={message}
+            onChange={(e) => {setMessage(e.target.value)
+                              console.log(message)}}
+            className='reportArea'
+            placeholder='Digite seu relato...'/>
+          <button className="reportSend" disabled={!sendEnabled}>Enviar Denúncia</button>
+        </form>
+        <div className="optionsInner">
+          <i class="fa-solid fa-chevron-left"></i>
+          <i class="fa-regular fa-circle-question"></i>
         </div>
-        <h1 className="reportTheme">CANAL DE DENÚNCIAS</h1>
-        <textarea className='reportArea' placeholder='Digite seu relato...'/>
-        <button className="reportSend" >Enviar Denúncia</button>
       </div>
     </div>
   )
